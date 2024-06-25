0a8584ae655a33158a1077c88183e9f69d79d5c3
Kapycta1488 <145847763+Kapycta1488@users.noreply.github.com>
2024-06-25 16:23:53 +0300

    Update d.lua

--- d.lua
+++ d.lua
@@ -2,7 +2,9 @@ password = 1234
 
 
 while true do
+  term.clear()
+  print('Write password')
   local pas = tonumber(read())
 
-  if pas == password then rs.setOutput('back', true); sleep(5); rs.setOutput('back', false) end
+  if pas == password then print('Succesfuly'); rs.setOutput('back', true); sleep(5); rs.setOutput('back', false) end
 end
